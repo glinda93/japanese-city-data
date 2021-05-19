997771666399a681a5cb762adcd7ff647689e89c
glinda93 <glinda7941@gmail.com>
2021-05-19 23:14:17 +0800

    Add finders

--- src/japanese-city-data.ts
+++ src/japanese-city-data.ts
@@ -6,5 +6,40 @@ export type CityData = {
   children?: CityData[];
 };
 
-const CityData: CityData[] = fixture;
-export default CityData;
+const Data: CityData[] = fixture;
+export default Data;
+
+export const getPrefId = (pref: string) => {
+    const prefItem = Data.find(item => {
+        item.label === pref;
+    });
+    if (prefItem) {
+        return prefItem.value;
+    }
+    return null;
+};
+
+export const getCityIdByPrefId = (prefId: number, city: string) => {
+    const prefItem = Data.find(item => {
+        item.value === prefId;
+    })
+    if (prefItem) {
+        const cityItem = prefItem.children?.find(item => {
+            item.label === city;
+        });
+        if (cityItem) {
+            return cityItem.value;
+        }
+        return null;
+    }
+    return null;
+};
+
+export const getPrefAndCityId = ({pref, city}: {pref: string, city: string}) => {
+    const prefId = getPrefId(pref);
+    if (prefId === null) {
+        return {prefId: null, cityId: null};
+    }
+    const cityId = getCityIdByPrefId(prefId, city);
+    return {prefId, cityId};
+};
